6594a6642d4bb23fbed7352960b81cc23f22fb76
Frankie Moran <frankie.jack.moran@gmail.com>
2025-10-21 14:18:41 +0100

    Add userStateCacheExpirationMilliseconds to the transfer-server-state

--- src/interfaces/transfer-server-state.ts
+++ src/interfaces/transfer-server-state.ts
@@ -5,6 +5,7 @@ class TransferServerState {
   launchDarklyConfig!: LaunchDarklyConfig;
   ssoEnabled!: boolean;
   appInsightsConfig!: AppInsightsConfig;
+  userStateCacheExpirationMilliseconds!: number;
 }
 
 export default TransferServerState;
