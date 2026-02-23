f6d30ad052d8456f6e062941766e9bcb8b7ab3c2
Frankie Moran <frankie.jack.moran@gmail.com>
2026-02-23 12:01:03 +0000

    Change IConfig to Config

--- src/properties-volume/index.ts
+++ src/properties-volume/index.ts
@@ -1,9 +1,9 @@
 import * as propertiesVolume from '@hmcts/properties-volume';
-import { IConfig } from 'config';
+import { Config } from 'config';
 import { Application } from 'express';
 
 export class PropertiesVolume {
-  enableFor(server: Application, config: IConfig): IConfig {
+  enableFor(server: Application, config: Config): Config {
     if (server.locals['ENV'] !== 'development') {
       propertiesVolume.addTo(config);
     }
