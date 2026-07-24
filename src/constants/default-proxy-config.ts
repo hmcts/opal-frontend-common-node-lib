d2d9574db1ac87aa72b301196e09c7c4856d24b5
TimDanielsCQI <275969854+TimDanielsCQI@users.noreply.github.com>
2026-07-24 09:19:19 +0100

    Update src/constants/default-proxy-config.ts
    Co-authored-by: Frankie Moran <frankie.jack.moran@gmail.com>

--- src/constants/default-proxy-config.ts
+++ src/constants/default-proxy-config.ts
@@ -4,5 +4,5 @@ export const DEFAULT_PROXY_CONFIG: Readonly<ProxyConfiguration> = Object.freeze(
   opalFinesServiceUrl: null,
   opalUserServiceUrl: null,
   opalRmServiceUrl: null,
-  timeoutInMilliseconds: 30000,
+  timeoutInMilliseconds: null,
 });
